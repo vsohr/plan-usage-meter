74c121efd118ed01b33d2cbf6eb87943ac374a27
vadim <vadim.ohrimenko@gmail.com>
2026-05-01 20:25:05 +0100

    feat(M2.T3): preload window.api surface

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,23 @@
 'use strict';
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
+
+const CH = Object.freeze({
+  USAGE_UPDATE:  'usage:update',
+  USAGE_REFRESH: 'usage:refresh',
+  WIN_HIDE:      'window:hide',
+  APP_QUIT:      'app:quit',
+  WIN_HEIGHT:    'window:report-height'
+});
 
 contextBridge.exposeInMainWorld('api', Object.freeze({
-  // M2 wires onUsage / refreshNow / hide / quit / reportHeight here.
-  __preloadVersion: '0.1.0'
+  onUsage(cb) {
+    if (typeof cb !== 'function') return () => {};
+    const handler = (_e, usage) => cb(usage);
+    ipcRenderer.on(CH.USAGE_UPDATE, handler);
+    return () => ipcRenderer.removeListener(CH.USAGE_UPDATE, handler);
+  },
+  refreshNow() { return ipcRenderer.invoke(CH.USAGE_REFRESH); },
+  hide()       { ipcRenderer.send(CH.WIN_HIDE); },
+  quit()       { ipcRenderer.send(CH.APP_QUIT); },
+  reportHeight(px) { ipcRenderer.send(CH.WIN_HEIGHT, px); }
 }));
